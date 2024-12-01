04dc149f448abe987a55a4e2ee2400585e671b50
ckim48 <madckkim@gmail.com>
2024-12-01 09:55:55 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -9,27 +9,27 @@ from textblob import TextBlob
 from config import firebaseConfig
 import pyrebase
 from collections import Counter
+import yake
 
 app = Flask(__name__) # create an empty web app
 
 firebase = pyrebase.initialize_app(firebaseConfig)
 db = firebase.database()
-
 def analyze_sentiment(user_input):
   analysis = TextBlob(user_input)
   sentiment = analysis.sentiment.polarity
   return sentiment
-
+lst = ["How do you feel about this game?", "Have you purchased any in-game purchasing item?","How do you feel about the in-game purchases?", "Do you feel that the prices for in-game purchases are fair?", "What type of in-game purchases do you enjoy the most?", "What is your gender?", "What is your age?"]
 def chatbot():
+  extractor = yake.KeywordExtractor(lan="en", n=1, top=3) # we are going to extract the 3 most important words
   print("Hello! I'm a chatbot.")
-  print("How you feel about this game?") # Game A
-  while True:
-    user_input = input("User: ")
-    sentiment = analyze_sentiment(user_input)
-
-    data = {"user_answer":user_input, "sentiment":sentiment}
-    db.child("chatbot").push(data)
-
+  for i in range(len(lst)): # 0 ~ 2
+    print(lst[i])
+    user_input = input("User: ") # I love it
+    keywords = extractor.extract_keywords(user_input)
+    sentiment = analyze_sentiment(user_input) # 0.5
+    data = {"question_number": i+1, "user_answer":user_input, "sentiment":sentiment, "keywords": keywords}
+    db.child("chatbot").push(data) # Adding the data into our database
     print(sentiment)
 # Firebase Database
 # Feedback
@@ -38,13 +38,18 @@ def chatbot():
 # - index
 @app.route('/dashboard')
 def dashboard():
-    feedbacks = db.child("feedbacks").get().val().values()
-    sentiments = [feedback["sentiment"] for feedback in feedbacks] # ["Positive","Negative","Positive"...]
-    sentiments_counts = Counter(sentiments) # {"Positive": 50, "Negative": 20, "Neutral" :10}
-    sentiments_labels = list(sentiments_counts.keys()) # ["Positive", "Negative", "Neutral"]
-    sentiments_data = list(sentiments_counts.values()) # [50, 20, 10]
-
-    return render_template('dashboard.html',sentiments_labels=sentiments_labels,sentiments_data=sentiments_labels )
+    feedbacks = db.child("chatbot").get().val().values() # every data under document 'chatbot'
+    sum_sentiment = 0
+    count = 0
+    for feedback in feedbacks:
+        # print(feedback)
+        if feedback["question_number"] != 2 and feedback["question_number"] != 6 and feedback["question_number"] != 7:
+            sum_sentiment += feedback["sentiment"]
+            count += 1
+    average_sentiment = sum_sentiment/count
+    print("Average Sentiment", average_sentiment)
+    return render_template('dashboard.html', average_sentiment = average_sentiment)
 
 if __name__ == "__main__":
+    # chatbot()
     app.run(debug=True) # running web-app
